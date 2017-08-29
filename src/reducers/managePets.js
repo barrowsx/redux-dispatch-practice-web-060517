0bc0194b72e772eabb4fdbad22a70a2fd2e75dbd
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-29 17:25:50 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,30 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch(action.type){
+    case "ADD_PET":
+      if(action.pet.name !== undefined && action.pet.species !== undefined && action.pet.id !== undefined){
+        return {...state, pets: [...state.pets, action.pet]}
+      } else {
+        return state
+      }
+    case "REMOVE_PET":
+      return {...state, pets: state.pets.filter(element => {
+        return element.id !== action.id
+      })}
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let container = document.getElementById('container')
+  container.innerHTML = `<ul><li>Splash</li></ul>`
+  // this test is awful btw
 }
